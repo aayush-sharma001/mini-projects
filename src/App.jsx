b11e9a79f662568ab8eede07e8bd881d4eafec8a
aayush-sharma001 <aayush001sharma@gmail.com>
2025-05-24 20:35:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full min-h-screen flex flex-col items-center justify-center bg-black w-100">
+        <p className="text-white text-xl font-semibold">Mini Projects Using React and Tailwind</p>
+      </div>
+    </>
+  )
+}
+
+export default App;
